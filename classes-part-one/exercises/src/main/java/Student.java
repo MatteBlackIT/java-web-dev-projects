cc9c00f6c5aeecee01f3b9b88e5aa30cfd3359e9
MatteBlackIT <bouwkamp.IT@gmail.com>
2024-05-14 02:45:10 +0900

    getter/setters COMPLETE

--- classes-part-one/exercises/src/main/java/Student.java
+++ classes-part-one/exercises/src/main/java/Student.java
@@ -1,6 +1,40 @@
 public class Student {
         private String name;
+
+        public String getName() {
+                return name;
+        }
+
         private int studentId;
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public int getStudentId() {
+                return studentId;
+        }
+
+        public void setStudentId(int studentId) {
+                this.studentId = studentId;
+        }
+
+        public int getNumberOfCredits() {
+                return numberOfCredits;
+        }
+
+        public void setNumberOfCredits(int numberOfCredits) {
+                this.numberOfCredits = numberOfCredits;
+        }
+
+        public double getGpa() {
+                return gpa;
+        }
+
+        public void setGpa(double gpa) {
+                this.gpa = gpa;
+        }
+
         private int numberOfCredits = 0;
         private double gpa = 0.0;
 
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,52 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+
+        Student student = new Student("MatteBlack", 12345, 1, 4.0);
+    }
+    public class Course {
+        private String topic;
+        private Teacher instructor;
+        private ArrayList<Student> enrolledStudents;
+    }
+
+    public class Teacher {
+        public String firstName;
+        public String lastName;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getSubject() {
+            return subject;
+        }
+
+        public void setSubject(String subject) {
+            this.subject = subject;
+        }
+
+        public int getNumOfYears() {
+            return numOfYears;
+        }
+
+        public void setNumOfYears(int numOfYears) {
+            this.numOfYears = numOfYears;
+        }
+
+        public String subject;
+        public int numOfYears;
     }
 }
